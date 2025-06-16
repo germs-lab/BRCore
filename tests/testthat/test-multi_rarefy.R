c552ba3e5727d43ace9631bfac5d4abda897f0a7
jibarozzo <bolaponte@pm.me>
2025-06-16 06:55:57 -0500

    Fix typo

--- tests/testthat/test-multi_rarefy.R
+++ tests/testthat/test-multi_rarefy.R
@@ -1,15 +1,16 @@
 test_that("rarefaction worked correctly", {
-    
     # Load expected
     load(here::here("tests/data/test_phyloseq.rda"))
     
-
-    otu_table_rare <-
-        multi_rarefy(physeq = test_phyloseq,
-                             depth_level = 500,
-                             num_iter = 10,
-                     set_seed = 1234))
     
-    # Test if all the samples have the same number of reads
-    expect_true(all(rowSums(otu_table_rare) == rowSums(otu_table_rare)[1]))
+    otu_table_rare <-
+        multi_rarefy(
+            physeq = test_phyloseq,
+            depth_level = 500,
+            num_iter = 10,
+            set_seed = 1234
+        )
+
+# Test if all the samples have the same number of reads
+expect_true(all(rowSums(otu_table_rare) == rowSums(otu_table_rare)[1]))
 })
