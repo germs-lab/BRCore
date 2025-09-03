9f0871b7627d261e99356c65be76ab7e03585908
GianBen <gian.benucci@gmail.com>
2025-09-03 13:26:17 -0400

    new tests (#30)
    * new tests
    
    * update and check again
    
    * Changes to be committed:modified:   NAMESPACE new file:   man/do_phyloseq.Rd
    
    * Changes to be committed:
    	modified:   NAMESPACE
    	modified:   tests/testthat/test-multi_rarefy.R
    
    * Changes to be committed:
    	modified:   tests/testthat/test-do_phyloseq.R
    
    * new push with hopefully fixed problems
    
    * test passed  for ubunutu not for macos...
    
    * last try before changing strategy...
    
    * trying the /dontrun{} option and see if it pass the tests...
    
    * trying again...
    
    * forgot NAMESPACE...
    
    * Removing error" Namespace dependencies missing from DESCRIPTION Imports/Depends entries:
        'furrr', 'tidyverse'" introduced by undeclared exports and mismatch between NAMESPACE and DESCRIPTION.
    
    * Adding set_seed argument to ensure reproducibility across OS.
    
    * Fix typo
    
    * Update test-do_phyloseq.R
    
    Debugging
    
    * Changed test to have hardcoded value.
    
    * Hopefully fixing test check fails in GH actions due to OS differences.
    
    * Expecting TRUE is not working.
    
    * Making sure it counts read depth
    
    ---------
    
    Co-authored-by: jibarozzo <bolaponte@pm.me>
    Co-authored-by: Bolívar Aponte Rolón <64099468+jibarozzo@users.noreply.github.com>

--- R/do_phyloseq.R
+++ R/do_phyloseq.R
@@ -1,17 +1,16 @@
-#' Import a rarefied otu_table into a phyloseq object
+#' Add a rarefied otu_table to a phyloseq object
 #'
 #' This function imports a rarefied otu_table (or normalized) in a `phyloseq` object `otu_table()` layer.
 #'
-#' @param physeq A `phyloseq` object in which I want to add the rarefied OTU/ASV table.
+#' @param physeq A `phyloseq` object in which you want to add the rarefied OTU/ASV table.
 #' @param otu_rare A rarefied otu_table dataframe.
 #'
-#' @return A `phyloseq` object. 
-#' 
+#' @return A `phyloseq` object.
+#'
 #' @import phyloseq
 #' @import vegan
 #' @import dplyr
 #' @import tibble
-#' @export
 #'
 #' @examples
 #' \dontrun{
@@ -19,12 +18,17 @@
 #' data(GlobalPatterns, package = "phyloseq")
 #'
 #' # Perform multiple rarefaction
-#' rarefied_data <- multi_rarefy(GlobalPatterns, depth_level = 1000, num_iter = 99)
-#' head(rarefied_data)
-#' 
+#' rarefied_data <- multi_rarefy(GlobalPatterns, depth_level = 500, num_iter = 99)
+#'
+#' # Check the rarefied data output
+#'rowSums(rarefied_data)
+#'
+#' # Recreate the phyloseq object and check
 #' rarefied_physeq <- do_phyloseq(physeq = GlobalPatterns, otu_rare=rarefied_data )
 #' head(otu_table(rarefied_physeq))
 #' }
+#'
+#' @export
 do_phyloseq <- function(physeq, otu_rare) {
     new_phyloseq <-
         phyloseq(
@@ -40,6 +44,6 @@ do_phyloseq <- function(physeq, otu_rare) {
         ) %>%
         prune_taxa(taxa_sums(x = .) > 0, x = .) %>%
         prune_samples(sample_sums(x = .) > 0, x = .)
-    
+
     return(new_phyloseq)
 }
---
+++ tests/testthat/test-multi_rarefy.R
@@ -0,0 +1,18 @@
+test_that("rarefaction worked correctly", {
+    # Load expected
+    load(here::here("tests/data/test_phyloseq.rda"))
+    
+    
+    otu_table_rare <-
+        multi_rarefy(
+            physeq = test_phyloseq,
+            depth_level = 500,
+            num_iter = 10,
+            set_seed = 1234
+        )
+    
+    # Test if all the samples have the same number of reads
+    read_counts <- rowSums(otu_table_rare)
+    expect_equal(max(read_counts) - min(read_counts), 0) # All values are identical
+    
+})
