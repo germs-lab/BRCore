3be2ee22e3732573e99bc72daac8c789d56fcdb3
Bolívar Aponte Rolón <64099468+jibarozzo@users.noreply.github.com>
2025-09-29 09:02:41 -0500

    Formatted global variables to globals.R

--- R/globals.R
+++ R/globals.R
@@ -1,10 +1,26 @@
 utils::globalVariables(
     c(
-        ".", "sample_id",
-        "fill", "grp", "otu", "otu_occ", "otu_rel", "p",
-        "pred.lwr", "pred.upr", "proportionBC", "rank_num",
-        "sumF", "sumG","nS", "Index", "MeanBC", "elbow", "IncreaseBC", "freq.pred",
-        "time_freq"
+        ".",
+        "NMDS1",
+        "NMDS2",
+        "Dim1",
+        "Dim2",
+        "SampleID",
+        "sample_id",
+        "abun",
+        "time_freq",
+        "coreTime",
+        "sumF",
+        "sumG",
+        "nS",
+        "Index",
+        "comparison",
+        "BC",
+        "MeanBC",
+        "elbow",
+        "IncreaseBC",
+        "freq.pred"
     )
 )
+
 utils::globalVariables(c("%>%", "%T>%"))
