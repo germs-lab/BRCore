658dcd608b382e354722a88c2d55e43348e20c03
jibarozzo <bolaponte@pm.me>
2026-04-14 15:22:52 -0500

    fix: separate local and CI test data path logic in setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -3,8 +3,15 @@
 rda_path <- test_path("test_sets/test_vignette_data.rda")
 
 # Only regenerate if the .rda is missing or if running on CI
-if (!file.exists(rda_path) || nzchar(Sys.getenv("CI"))) {
-  rda_path <- file.path(tempdir(), "test_vignette_data.rda")
+if (!file.exists(rda_path)) {
   source(test_path("test_sets/regenerate_test_data.R"))
+}
+
+if (nzchar(Sys.getenv("CI"))) {
+  rda_path <- file.path(
+    tempdir(),
+    test_path("test_sets/test_vignette_data.rda")
+  )
   options(brcore_test_data_path = rda_path)
+  source(rda_path, local = TRUE)
 }
